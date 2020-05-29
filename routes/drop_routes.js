781d3150c16c4e5511990a1ba5b3648f33e0f07d
czara804 <cbzara@gmail.com>
2020-05-29 11:53:54 +1000

    seed-data

--- routes/drop_routes.js
+++ routes/drop_routes.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const router = express.Router()
-const {makeDrop} = require("../controllers/drops_controller")
+const {makeDrop, getDrops, getDrop, removeDrop, changeDrop} = require("../controllers/drops_controller")
 
 // view all drops
-// router.get("/", getDrops)
+router.get("/", getDrops)
 
 // // view individual drop
-// router.get("/:id", getDrop)
+router.get("/:id", getDrop)
 
 // // make new drop 
 // //CREATE
 router.post("/", makeDrop)
 
 // //delete drop 
-// router.delete("/:id", removerDrop)
+router.delete("/:id", removeDrop)
 
 // // update drop
-// router.put("/:id", changeDrop)
+router.put("/:id", changeDrop)
 
 module.exports = router;
 
